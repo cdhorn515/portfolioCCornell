1024489f6cdee4dc17f2ac3e9d4aa74ba8e76f55
Christina Cornell <cdhorn515@gmail.com>
2017-07-27 21:34:04 -0400

    Initial commit, able to see html at index page

---
+++ src/main/java/com/cdhorn/Controllers/Home.java
@@ -0,0 +1,21 @@
+package com.cdhorn.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+
+    @RequestMapping("/")
+    public String index(ModelMap model) {
+
+
+
+
+        return "index";
+
+    }
+
+}
---
+++ src/main/java/com/cdhorn/PortfolioApplication.java
@@ -0,0 +1,12 @@
+package com.cdhorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+}
