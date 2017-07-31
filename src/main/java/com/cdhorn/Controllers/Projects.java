5790efd0257583d5b3886cb6ee35bf625d803c94
Christina Cornell <cdhorn515@gmail.com>
2017-07-31 17:36:39 -0400

    projects page is now rendering projects from information in database.

--- src/main/java/com/cdhorn/Controllers/Projects.java
+++ src/main/java/com/cdhorn/Controllers/Projects.java
@@ -1,17 +1,24 @@
 package com.cdhorn.Controllers;
 
+import com.cdhorn.Models.Project;
+import com.cdhorn.Repositories.ProjectRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class Projects {
 
-//    @Autowired
-//    private ProjectRepositoryImpl repo;
+    @Autowired
+    private ProjectRepositoryImpl repo;
 
     @RequestMapping("/projects")
     public String projects(Model model) {
+        List<Project> allProjects = repo.findAll();
+        model.addAttribute("projects", allProjects);
 
 
         return "projects";
