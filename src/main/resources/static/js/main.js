b7674a9c9b87e820976795cd94a0981ed9bfc1b4
Christina Cornell <cdhorn515@gmail.com>
2017-08-15 23:43:41 -0400

    added conditional statement for collapsible, fixed footer issue

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,19 @@
 (function(){
 
-
     "use strict";
 
+    var counter = 0;
+
+    var aboutMeBioNode = document.getElementById("aboutMeContainer");
+    aboutMeBioNode.addEventListener("click", function() {
+        if (counter % 2===0) {
+
+        aboutMeBioNode.setAttribute("style", "width: 45%");
+        } else {
+            aboutMeBioNode.setAttribute("style", "width: 25%");
+        }
+        counter++;
+    })
 
 
 })();
