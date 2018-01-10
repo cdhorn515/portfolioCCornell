541d28047516133d9f08e5785bcd065682e075e6
Christina Cornell <cdhorn515@gmail.com>
2018-01-10 18:51:38 -0500

    Working with junit, added a couple basic tests for home page

---
+++ src/test/java/com/cdhorn/ControllerTests/HomeTests.java
@@ -0,0 +1,30 @@
+package com.cdhorn.ControllerTests;
+
+import com.cdhorn.Controllers.Home;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HomeTests {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new Home()).build();
+    }
+
+    @Test
+    public void testHomePageStatus() throws Exception {
+
+        this.mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+    }
+}
--- src/test/java/com/cdhorn/PortfolioApplicationTests.java
+++ src/test/java/com/cdhorn/PortfolioApplicationTests.java
@@ -3,14 +3,22 @@ package com.cdhorn;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class PortfolioApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void sanityTest() throws Exception {
+		assertEquals(3, 3);
+	}
+
 
 }
