aa04f529e5a1ed432bd46ac79db5480d0a1b1a20
Christina Cornell <cdhorn515@gmail.com>
2018-01-11 17:32:30 -0500

    test to check projects endpoint still failing

--- src/test/java/com/cdhorn/ControllerTests/ProjectsTests.java
+++ src/test/java/com/cdhorn/ControllerTests/ProjectsTests.java
@@ -1,27 +1,46 @@
 package com.cdhorn.ControllerTests;
 
 
+import com.cdhorn.Controllers.Projects;
+import com.cdhorn.Interfaces.ProjectRepository;
 import com.cdhorn.PortfolioApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = PortfolioApplication.class)
+@ActiveProfiles(profiles = "test")
 public class ProjectsTests {
 
     private MockMvc mockMvc;
 
     @Autowired
+    ProjectRepository projectRepoTest;
+
+    @Autowired
     private GenericEntityRepository genericEntityRepository;
-    
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new Projects())
+                .addPlaceholderValue("project", "1")
+                .build();
+    }
+
     @Test
     public void givenGenericEntityRepository_whenSaveAndRetrieveEntity_thenOK() {
         GenericEntity genericEntity = genericEntityRepository
@@ -33,4 +52,11 @@ public class ProjectsTests {
         assertEquals(genericEntity.getValue(), foundEntity.getValue());
     }
 
+    @Test
+    public void testProjectsPageStatus() throws Exception {
+
+        mockMvc.perform(get("/projects"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("project"));
+    }
 }
